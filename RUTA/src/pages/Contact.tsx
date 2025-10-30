c367e4cfc8c112d1814106f3bac3836d034ea4fb
Ana Sofia M.M <anasmoli05@gmail.com>
2025-10-30 07:45:43 -0500

    tras archivos nuevos

---
+++ RUTA/src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+function Contact(){
+
+    return(
+        <div>
+             <h1>Contact Us</h1>
+        <p>This is sthe contact us of our application</p>
+        </div>
+       
+    );
+}
+
+export default Contact;
---
+++ RUTA/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home(){
+
+    return(
+        <div>
+            <h1>Bienvenido clase UNAC</h1>
+            <p>Esta pagina es pARA LOS COMPAÑEROS PUEDAN INGRESAR E instruierse con el proyecto</p>
+            <p>Espero les guste y puedan aprender mucho</p>
+        </div>
+    );
+}
+
+export default Home;
